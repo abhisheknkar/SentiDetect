848c46311d34e0ac7cdd8c094561b0c2b3dea091
abhishek <abhisheknkar@gmail.com>
2015-06-08 16:42:30 +0530

    First Commit

---
+++ GenericSentiDetect/src/ReadSentiLexicon.java
@@ -0,0 +1,108 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author abhishek
+ *
+ */
+public class ReadSentiLexicon 
+{
+	public static void main(String[] args) throws IOException
+	{
+//		String InputSentence = "I abuse you and hate you very much";
+//		String InputSentence = "I love you and adore you very much";
+		String InputSentence = "I am feeling awesome and horrible simultaneously";
+		int PolarityThreshold = 1;
+		int DisplayIndividualScores = 1;
+		
+		long startTime = System.currentTimeMillis();
+		int saveFlag = 0;
+		int sentenceScore;
+		File fin = new File("./senti.txt");
+		File fout = new File("./OpinionFinderWordList.tmp");
+
+		ArrayList<OpinionFinderWord> Words = new ArrayList<OpinionFinderWord>();
+		Words = readDataset_OpinionFinder(fin,fout, saveFlag);	//Create the list of words
+		//Given sentence, parse each word, see if it exists in the list. If it does, add its score to aggregate.
+
+		sentenceScore = computeScore_OpinionFinder(Words, InputSentence, PolarityThreshold, DisplayIndividualScores);
+		
+		long endTime   = System.currentTimeMillis();
+		long totalTime = endTime - startTime;
+		System.out.println("Total time: " + totalTime + "ms.");		
+	}
+	
+	public static  ArrayList<OpinionFinderWord> readDataset_OpinionFinder(File fin, File fout, int saveFlag) throws IOException
+	{		
+		BufferedReader in = null;
+		in = new BufferedReader(new FileReader(fin));
+		
+		String line = null;
+		String[] temp1;
+		String[] temp2;
+		String[] temp3 = new String[6];
+		
+		ArrayList<OpinionFinderWord> Words = new ArrayList<OpinionFinderWord>();
+		
+		while((line = in.readLine()) != null)
+		{
+			temp1 = line.split(" ");
+			
+			for(int i=0; i<6; ++i)
+			{
+				temp2 = temp1[i].split("=");
+				temp3[i] = temp2[1];
+			}
+			Words.add(new OpinionFinderWord(temp3[0],temp3[1],temp3[2],temp3[3],temp3[4],temp3[5]));			
+		}
+		if (saveFlag != 0) 
+		{
+			FileOutputStream foutStream = new FileOutputStream(fout);
+			ObjectOutputStream oos = new ObjectOutputStream(foutStream);
+			oos.writeObject(Words);		
+		}
+		return Words;
+	}
+
+	public static int computeScore_OpinionFinder(ArrayList<OpinionFinderWord> Words, String InputSentence, int PolarityThreshold, int DisplayIndividualScores)
+	{
+		int Score = 0;
+		int Add2Score = 0;
+		int Sign = 0;
+		String[] temp = InputSentence.split(" ");
+		
+		if (DisplayIndividualScores != 0) System.out.println("Term:  Score:");
+		
+		for (int i = 0; i < temp.length; ++i)
+		{
+			for (ListIterator<OpinionFinderWord> iter = Words.listIterator(); iter.hasNext(); ) 
+			{
+			    OpinionFinderWord X = iter.next();
+
+				Add2Score = 0;
+				Sign = 0;
+				if (temp[i].equals(X.Term))
+				{
+					if (X.Type.equals("strongsubj")) Add2Score += 2;
+					else if (X.Type.equals("weaksubj")) Add2Score += 1;
+
+					if (X.PriorPolarity.equals("positive")) Sign = 1;
+					else if (X.PriorPolarity.equals("negative")) Sign = -1;
+					else Sign = 0;
+					
+					Add2Score *= Sign;
+					Score += Add2Score;
+						
+					if (DisplayIndividualScores != 0) System.out.println(X.Term + "  " + Add2Score);
+						
+					break;
+				}
+			}
+		}
+		System.out.println("Sentence Score is " + Score);
+		if (Score > PolarityThreshold) System.out.println("The sentence is positive.");
+		else if (Score < -PolarityThreshold) System.out.println("The sentence is negative.");
+		else System.out.println("The sentence is neutral.");
+		return Score;
+	}
+}
