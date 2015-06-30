efbe500bd54490c5ea41d2da4dd8b97ae9dd9cc0
abhishek <abhisheknkar@gmail.com>
2015-06-30 19:43:44 +0530

    Added name types.

--- GenericSentiDetect/src/Test1.java
+++ GenericSentiDetect/src/Test1.java
@@ -15,16 +15,27 @@ public class Test1 {
 	{
 		// TODO Auto-generated method stub
 
-		String name = "[A-Z][A-Za-z]+([ \\n][A-Za-z][-A-Za-z\n\\.]+)+";
-		String whitespace = "[ \\n]";
-		String regex = name + "(,?" + whitespace + name + ")*,?" + whitespace + "?(and)?" + whitespace + "?" + "(" + name + ")?\\." + whitespace + "[0-9]{4}";
+		String nametype1 = "[A-Z][A-Za-z]*([ \r\n]+[A-Za-z]\\.?[-A-Za-z\n]+){1,5}";	//<FN> <LN>
+		String nametype2 = "[A-Z][A-Za-z]*,([ \r\n]+[A-Za-z]\\.?[-A-Za-z\n]+){1,5}"; //<LN>, <FN>			
+		String nametype3 = "([A-Z][\\. \r\n]+)+([A-Za-z][-A-Za-z\n]+){1,5}"; //I. <LN>			
+		String nametype4 = "([A-Za-z][-A-Za-z\n]+)+[, \r\n]+([A-Z][\\. \r\n])+"; //<LN>, I.			
+
+		String name = "(" + nametype1 + "|" + nametype2 + "|" + nametype3 + "|" + nametype4 + ")";
+		String whitespace = "[ \r\n]*";
+		
+		String nameoccurrence1 = name;
+		String nameoccurrencex = "(,?" + whitespace + name + ")*";
+		String nameoccurrencelast = ",?" + whitespace + "(and)?" + whitespace + "(" + name + ")?[\\.]" + whitespace;
+		String yearoccurrence = "[0-9]{4}[A-Za-z]*"; 		
+		
+		String regex = nameoccurrence1 + nameoccurrencex + nameoccurrencelast + yearoccurrence;
 				
 //		String content = "Jean Mulder, Kate Burridge, Stanley Simoes, and\nCaroline\nThomas. 2001.";
 //		String content = "Simon Simon, Quintin Cutts, Sally Fincher, Patricia Haden, Anthony Robins, Ken Sutton, Bob Baker, Ilona\nBox, Michael de Raadt, John Hamer, Margaret Hamil-\nton, Raymond Lister, Marian Petre, Denise Tolhurst, and Jodi Tutty. 2006.";
 		HashMap <String, Paper> papers = DatasetReader.readAANMetadata();
 		int refStart = 0;
 		
-		for (Map.Entry<String, Paper> entry : papers.entrySet())
+//		for (Map.Entry<String, Paper> entry : papers.entrySet())
 		{
 //			String filepath = "C:/Abhishek_Narwekar/Papers,Datasets/Citation Polarity/Datasets/AAN/aan/papers_text/" + entry.getValue().id + ".txt";
 			String filepath = "Datasets/AAN/testpaper.txt";
@@ -37,7 +48,7 @@ public class Test1 {
 				Matcher matcher = pattern.matcher(content);
 				while(matcher.find())
 				{
-					System.out.println("matched \"" + matcher.group() + "\" at position " + matcher.start());
+					System.out.println("matched \"" + matcher.group() + "\"");// at position " + matcher.start());
 //					matchedAt = matcher.start();
 				}
 			}		
